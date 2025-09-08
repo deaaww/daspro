cfb3f321ad6c4daf0f5d23f62899995d37f156ce
deaaww <deamarsell12@gmail.com>
2025-09-08 21:02:11 +0700

    Squashed 'daspro-jobsheet3/' content from commit 29dbcf0
    git-subtree-dir: daspro-jobsheet3
    git-subtree-split: 29dbcf04bfe18bc98d01af04d91e374329c67f37

---
+++ Siakad08.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Siakad08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String nama, nim;
+        char kelas;
+        byte absen;
+        double nilaiKuis,nilaiTugas,nilaiUTS,nilaiUAS,nilaiAkhir;
+
+        System.out.println("Masukkan nama: ");
+        nama = sc.nextLine();
+        System.out.println("Masukkan NIM: ");
+        nim = sc.nextLine();
+        System.out.println("Masukkan kelas: ");
+        kelas = sc.nextLine().charAt(0);
+        System.out.println("Masukkan nomor absen: ");
+        absen = sc.nextByte();
+
+        System.out.print("Masukkan nilai kuis: ");
+        nilaiKuis = sc.nextDouble();
+        System.out.print("Masukkan nilai tugas: ");
+        nilaiTugas = sc.nextDouble();
+        System.out.print("Masukkan nilai UTS: ");
+        nilaiUTS = sc.nextDouble();
+        System.out.print("Masukkan nilai UAS: ");
+        nilaiUAS = sc.nextDouble();
+
+        nilaiAkhir = (nilaiKuis * 0.20) + (nilaiTugas * 0.15) + (nilaiUTS * 0.30) + (nilaiUAS * 0.35) / 3;
+
+        System.out.println("Nama: " + nama + " NIM: " + nim);
+        System.out.println("Kelas: " + kelas + " Absen: " + absen);
+        System.out.println("Nilai Akhir: " + nilaiAkhir);
+
+        sc.close();
+    }
+}
